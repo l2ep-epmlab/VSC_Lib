d580138021444a6241d4c53db919e0d7eae9f5ff
Frédéric Colas <frederic.colas@ensam.eu>
2021-06-17 17:39:06 +0200

    Big update
    - New mask with a better presentation and advanced parameters option
    - LCL Filter has now an option to simulate with real switches
    - Grid-Forming strategies has been simplified
    - Models with DC link are included
    - Bug fix in dq transformations
    - AC direct control is used
    - Current limitation based on inverse current loop has been added
    - File are available up to R2020b
    - Examples has been simplified
    - Measurement port has been modified to be more clear
    - LoadFlow script has been updated, it is now possible to connect several VSC on the same bus

---
+++ R2019a/Library/link_lib_sps.m
@@ -0,0 +1,37 @@
+model_list = libinfo(gcs,'IncludeCommented','on');
+
+find_lib = ["spsUniversalBridgeLib", 
+    "spsThreePhaseBreakerLib", 
+    "spsThreePhaseVIMeasurementLib", 
+    "spsSecondOrderFilterLib",
+    "spsSeriesRLCBranchLib",
+    "spsGroundLib",
+    "spsThreePhaseSeriesRLCBranchLib",
+    "spsSourcesModel",
+    "spsBreakerModel",
+    "spsThreePhaseTransformerTwoWindingsLib",
+    "spsThreePhaseSourceLib"];
+
+replace_lib = ["powerlib/Power Electronics",
+    "powerlib/Elements",
+    "powerlib/Measurements",
+    "powerlib_meascontrol/Filters",
+    "powerlib/Elements",
+    "powerlib/Elements",
+    "powerlib/Elements",
+    "powerlib/Electrical Sources",
+    "powerlib/Elements",
+    "powerlib/Elements",
+    "powerlib/Electrical Sources"];
+
+for i=1:length(model_list)
+    if contains(model_list(i).LinkStatus,'unresolved')
+        for j=1:length(find_lib)
+            if contains(model_list(i).Library,find_lib(j))
+                block_name = model_list(i).Block;
+                str = replace(string(model_list(i).ReferenceBlock),find_lib(j),replace_lib(j))
+                set_param(block_name,'SourceBlock',str);
+            end 
+        end
+    end
+end
